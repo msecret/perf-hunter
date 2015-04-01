1ed5143889d5f2be20caa88884b439b7e4e863e7
Marco Secret <msecret@localhost.localdomain>
2015-03-31 17:02:57 -0700

    Setup a send method
    Pretty much just a skeleton for later functionality.
    
    Tests don't work because duo can't pull in sinon. Duo is dissapointing,
    don't use again.

--- test/unit/index_test.js
+++ test/unit/index_test.js
@@ -1,5 +1,6 @@
 describe('perf-hunter', function(){
   var assert = require('component/assert');
+  var sinon = require('ianstormtaylor/sinon');
   var hunter = require('../../src/perf-hunter');
 
   describe('hunt()', function() {
@@ -22,6 +23,16 @@ describe('perf-hunter', function(){
       var actual = hunter.hunt(fakeWindow);
       assert(actual.performanceTiming);
       assert(typeof actual.performanceTiming === 'object');
-    })
+    });
+  });
+
+  describe('send()', function() {
+    it('should call passed in function with data as param', function() {
+      var spy = sinon.spy(),
+          expected = {test: 'data'};
+      hunter.send(expected, spy);
+
+      assert(spy.calledOnce);
+    });
   });
 });
